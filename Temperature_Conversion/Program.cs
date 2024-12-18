311816ba420914a92accb0810a87d4098526b4cb
Alexander T. <167206272+Cyb3rknight@users.noreply.github.com>
2024-12-17 23:10:51 -0500

    FCT Inital commit

---
+++ Temperature_Conversion/Program.cs
@@ -0,0 +1,10 @@
+﻿// Prompt the user to enter the temperature in degrees Celsius
+Console.WriteLine("Degré Celsius : "); 
+
+// Read the user's input and parse it as an integer
+int Celsius = int.Parse(Console.ReadLine()) ;
+
+// Calculate the temperature in degrees Fahrenheit using the formula:
+// Fahrenheit = (Celsius * 9/5) + 32
+// Then, use string interpolation to display the result, appending the "°F" symbol
+Console.WriteLine($"Degré Fahrenheit : {(Celsius * 9/5)+32 }°F");
